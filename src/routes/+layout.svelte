8031e5cbb4c6e8080008e7716576d8f900ef18bc
Kai Pereira <kaipereira2020@gmail.com>
2026-08-31 12:28:55 -0400

    basic styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 
+	import '../global.css'
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<header>
+	<h1>The Basics of ASICS</h1>
+	<p>An initiative to get teenagers designing and manufacturing their own ASIC's and chips by <a href="https://kaipereira.com/" target="_blank">Kai Pereira</a> and <a href="https://hackclub.com/" target="_blank">Hack Club!</a>
+</header>
+<nav>
+	<a href="/phase-1">Phase 1 (Design)</a>
+	<a href="/phase-2">Phase 2 (Emulation)</a>
+	<a href="/phase-3">Phase 3 (Fabrication)</a>
+</nav>
 
 {@render children()}
